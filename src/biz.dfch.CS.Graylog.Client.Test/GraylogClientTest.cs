39985e675d41ced1d4e91842a2f5dbaac0ec6414
Niklaus Jordi <jordi@afca.ch>
2016-01-11 13:32:15 +0100

    #1 Load Messages
    * Added overloads for search methods
    * Pass in optional parameters as a Parameters object
    
    resolves #1

--- src/biz.dfch.CS.Graylog.Client.Test/GraylogClientTest.cs
+++ src/biz.dfch.CS.Graylog.Client.Test/GraylogClientTest.cs
@@ -35,7 +35,7 @@ namespace biz.dfch.CS.Graylog.Client.Test
 
         [TestMethod]
         [TestCategory("SkipOnTeamCity")]
-        public void InvokeCoreNodeTemplateWithTenantIDSucceeds()
+        public void SearchMessagesTest()
         {
             //Test runs only if there is a file TestSettings.txt in the project folders containing the settings in the format
             //{key}={value}\n
@@ -49,7 +49,174 @@ namespace biz.dfch.CS.Graylog.Client.Test
             GraylogClient graylogClient = new GraylogClient();
             graylogClient.Login(Properties.Settings.Default.GraylogAPIUrl, username, password, TestEnvironment.TotalAttempts, TestEnvironment.BaseRetryIntervallMilliseconds);
 
-            dynamic messageCollection = graylogClient.SearchMessages(streamName, null, new DateTime(2015, 1, 1), new DateTime(2016, 1, 1), 0, 0, TestEnvironment.TotalAttempts, TestEnvironment.BaseRetryIntervallMilliseconds);
+            dynamic messageCollection = graylogClient.SearchMessages(streamName, new DateTime(2015, 1, 1), new DateTime(2016, 1, 1), TestEnvironment.TotalAttempts, TestEnvironment.BaseRetryIntervallMilliseconds);
+
+            Assert.IsNotNull(messageCollection, "No message collection received");
+            Assert.IsNotNull(messageCollection.messages, "No messages in message collection");
+            Assert.IsTrue(messageCollection.messages.Count > 0, "List of messages in message collection is empty");
+        }
+
+        [TestMethod]
+        [TestCategory("SkipOnTeamCity")]
+        public void SearchMessagesWithSortTest()
+        {
+            //Test runs only if there is a file TestSettings.txt in the project folders containing the settings in the format
+            //{key}={value}\n
+
+            // Arrange
+            TestSettings settings = TestSettings.Load();
+            string username = settings.GetValue("Username");
+            string password = settings.GetValue("Password");
+            string streamName = settings.GetValue("StreamName");
+
+            GraylogClient graylogClient = new GraylogClient();
+            graylogClient.Login(Properties.Settings.Default.GraylogAPIUrl, username, password, TestEnvironment.TotalAttempts, TestEnvironment.BaseRetryIntervallMilliseconds);
+
+            SearchParameters parameters = new SearchParameters()
+            {
+                SortFieldName = "hostname",
+                SortOrder = SortOrder.Ascending,
+            };
+            dynamic messageCollection = graylogClient.SearchMessages(streamName, new DateTime(2015, 1, 1), new DateTime(2016, 1, 1), parameters, TestEnvironment.TotalAttempts, TestEnvironment.BaseRetryIntervallMilliseconds);
+
+            Assert.IsNotNull(messageCollection, "No message collection received");
+            Assert.IsNotNull(messageCollection.messages, "No messages in message collection");
+            Assert.IsTrue(messageCollection.messages.Count > 0, "List of messages in message collection is empty");
+        }
+
+        [TestMethod]
+        [TestCategory("SkipOnTeamCity")]
+        public void SearchMessagesWithFieldListTest()
+        {
+            //Test runs only if there is a file TestSettings.txt in the project folders containing the settings in the format
+            //{key}={value}\n
+
+            // Arrange
+            TestSettings settings = TestSettings.Load();
+            string username = settings.GetValue("Username");
+            string password = settings.GetValue("Password");
+            string streamName = settings.GetValue("StreamName");
+
+            GraylogClient graylogClient = new GraylogClient();
+            graylogClient.Login(Properties.Settings.Default.GraylogAPIUrl, username, password, TestEnvironment.TotalAttempts, TestEnvironment.BaseRetryIntervallMilliseconds);
+
+            SearchParameters parameters = new SearchParameters()
+            {
+                FieldNames = new List<string>() { "message", "hostname" },
+            };
+            dynamic messageCollection = graylogClient.SearchMessages(streamName, new DateTime(2015, 1, 1), new DateTime(2016, 1, 1), parameters, TestEnvironment.TotalAttempts, TestEnvironment.BaseRetryIntervallMilliseconds);
+
+            Assert.IsNotNull(messageCollection, "No message collection received");
+            Assert.IsNotNull(messageCollection.messages, "No messages in message collection");
+            Assert.IsTrue(messageCollection.messages.Count > 0, "List of messages in message collection is empty");
+        }
+
+        [TestMethod]
+        [TestCategory("SkipOnTeamCity")]
+        public void SearchMessagesWithPagingTest()
+        {
+            //Test runs only if there is a file TestSettings.txt in the project folders containing the settings in the format
+            //{key}={value}\n
+
+            // Arrange
+            TestSettings settings = TestSettings.Load();
+            string username = settings.GetValue("Username");
+            string password = settings.GetValue("Password");
+            string streamName = settings.GetValue("StreamName");
+
+            GraylogClient graylogClient = new GraylogClient();
+            graylogClient.Login(Properties.Settings.Default.GraylogAPIUrl, username, password, TestEnvironment.TotalAttempts, TestEnvironment.BaseRetryIntervallMilliseconds);
+
+            SearchParameters parameters = new SearchParameters()
+            {
+                Limit = 20,
+                Offset = 30,
+            };
+            dynamic messageCollection = graylogClient.SearchMessages(streamName, new DateTime(2015, 1, 1), new DateTime(2016, 1, 1), parameters, TestEnvironment.TotalAttempts, TestEnvironment.BaseRetryIntervallMilliseconds);
+
+            Assert.IsNotNull(messageCollection, "No message collection received");
+            Assert.IsNotNull(messageCollection.messages, "No messages in message collection");
+            Assert.IsTrue(messageCollection.messages.Count > 0, "List of messages in message collection is empty");
+            Assert.IsTrue(messageCollection.messages.Count <= 20, "To many messages returned");
+        }
+
+        [TestMethod]
+        [TestCategory("SkipOnTeamCity")]
+        public void SearchMessagesWithPagingParameterDictionaryTest()
+        {
+            //Test runs only if there is a file TestSettings.txt in the project folders containing the settings in the format
+            //{key}={value}\n
+
+            // Arrange
+            TestSettings settings = TestSettings.Load();
+            string username = settings.GetValue("Username");
+            string password = settings.GetValue("Password");
+            string streamName = settings.GetValue("StreamName");
+
+            GraylogClient graylogClient = new GraylogClient();
+            graylogClient.Login(Properties.Settings.Default.GraylogAPIUrl, username, password, TestEnvironment.TotalAttempts, TestEnvironment.BaseRetryIntervallMilliseconds);
+
+            object parameters = new Dictionary<string, object>()
+            {
+                { SearchParameters.LimitKey, 20 },
+                { SearchParameters.OffsetKey, 30 },
+            };
+            dynamic messageCollection = graylogClient.SearchMessages(streamName, new DateTime(2015, 1, 1), new DateTime(2016, 1, 1), parameters, TestEnvironment.TotalAttempts, TestEnvironment.BaseRetryIntervallMilliseconds);
+
+            Assert.IsNotNull(messageCollection, "No message collection received");
+            Assert.IsNotNull(messageCollection.messages, "No messages in message collection");
+            Assert.IsTrue(messageCollection.messages.Count > 0, "List of messages in message collection is empty");
+            Assert.IsTrue(messageCollection.messages.Count <= 20, "To many messages returned");
+        }
+
+        [TestMethod]
+        [TestCategory("SkipOnTeamCity")]
+        public void SearchMessagesWithQueryTest()
+        {
+            //Test runs only if there is a file TestSettings.txt in the project folders containing the settings in the format
+            //{key}={value}\n
+
+            // Arrange
+            TestSettings settings = TestSettings.Load();
+            string username = settings.GetValue("Username");
+            string password = settings.GetValue("Password");
+            string streamName = settings.GetValue("StreamName");
+
+            GraylogClient graylogClient = new GraylogClient();
+            graylogClient.Login(Properties.Settings.Default.GraylogAPIUrl, username, password, TestEnvironment.TotalAttempts, TestEnvironment.BaseRetryIntervallMilliseconds);
+
+            SearchParameters parameters = new SearchParameters()
+            {
+                Query = "message:\"PING~\"",
+            };
+            dynamic messageCollection = graylogClient.SearchMessages(streamName, new DateTime(2015, 1, 1), new DateTime(2016, 1, 1), parameters, TestEnvironment.TotalAttempts, TestEnvironment.BaseRetryIntervallMilliseconds);
+
+            Assert.IsNotNull(messageCollection, "No message collection received");
+            Assert.IsNotNull(messageCollection.messages, "No messages in message collection");
+            Assert.IsTrue(messageCollection.messages.Count > 0, "List of messages in message collection is empty");
+        }
+
+        [TestMethod]
+        [TestCategory("SkipOnTeamCity")]
+        public void SearchMessagesWithFilterTest()
+        {
+            //Test runs only if there is a file TestSettings.txt in the project folders containing the settings in the format
+            //{key}={value}\n
+
+            // Arrange
+            TestSettings settings = TestSettings.Load();
+            string username = settings.GetValue("Username");
+            string password = settings.GetValue("Password");
+            string streamName = settings.GetValue("StreamName");
+
+            GraylogClient graylogClient = new GraylogClient();
+            graylogClient.Login(Properties.Settings.Default.GraylogAPIUrl, username, password, TestEnvironment.TotalAttempts, TestEnvironment.BaseRetryIntervallMilliseconds);
+
+            SearchParameters parameters = new SearchParameters()
+            {
+                Filter = "streams:55b896c60cf20fc7dd52b273 AND message:\"PING\"",
+            };
+            dynamic messageCollection = graylogClient.SearchMessages(streamName, new DateTime(2015, 1, 1), new DateTime(2016, 1, 1), parameters, TestEnvironment.TotalAttempts, TestEnvironment.BaseRetryIntervallMilliseconds);
 
             Assert.IsNotNull(messageCollection, "No message collection received");
             Assert.IsNotNull(messageCollection.messages, "No messages in message collection");
--- src/biz.dfch.CS.Graylog.Client/GraylogClient.cs
+++ src/biz.dfch.CS.Graylog.Client/GraylogClient.cs
@@ -166,32 +166,196 @@ namespace biz.dfch.CS.Graylog.Client
 
         #region Messages
 
-        public DynamicJsonObject SearchMessages(string streamName, string query, DateTime from, DateTime to, int limit, int offset, int totalAttempts, int baseRetryIntervallMilliseconds)
+        /// <summary>
+        /// Searches for Messages
+        /// </summary>
+        /// <param name="streamName">The name of the stream in which to search messages</param>
+        /// <param name="from">From date for the search range</param>
+        /// <param name="to">To date for the search range</param>
+        public DynamicJsonObject SearchMessages(string streamName, DateTime from, DateTime to)
+        {
+            return this.SearchMessages(streamName, from, to, (SearchParameters)null, this.TotalAttempts, this.BaseRetryIntervallMilliseconds);
+        }
+
+        /// <summary>
+        /// Searches for Messages
+        /// </summary>
+        /// <param name="streamName">The name of the stream in which to search messages</param>
+        /// <param name="from">From date for the search range</param>
+        /// <param name="to">To date for the search range</param>
+        /// <param name="totalAttempts">Total attempts that are made for a request</param>
+        /// <param name="baseWaitingMilliseconds">Default base retry intervall milliseconds in job polling</param>
+        public DynamicJsonObject SearchMessages(string streamName, DateTime from, DateTime to, int totalAttempts, int baseRetryIntervallMilliseconds)
+        {
+            return this.SearchMessages(streamName, from, to, (SearchParameters)null, totalAttempts, baseRetryIntervallMilliseconds);
+        }
+
+        /// <summary>
+        /// Searches for Messages
+        /// </summary>
+        /// <param name="streamName">The name of the stream in which to search messages</param>
+        /// <param name="from">From date for the search range</param>
+        /// <param name="to">To date for the search range</param>
+        /// <param name="parameters">Optional query parameters</param>
+        /// <returns>The collection objec containing the found messages</returns>
+        public DynamicJsonObject SearchMessages(string streamName, DateTime from, DateTime to, object parameters)
+        {
+            Contract.Assert((parameters is SearchParameters) || (parameters is Dictionary<string, object>), "Parameters format is not suported (valid formats are 'SearchParameters' or 'Dictionary<string,object>'");
+
+            DynamicJsonObject result = null;
+            if (parameters is SearchParameters)
+            {
+                result = this.SearchMessages(streamName, from, to, (SearchParameters)parameters);
+            }
+            else if (parameters is Dictionary<string, object>)
+            {
+                result = this.SearchMessages(streamName, from, to, (Dictionary<string, object>)parameters);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Searches for Messages
+        /// </summary>
+        /// <param name="streamName">The name of the stream in which to search messages</param>
+        /// <param name="from">From date for the search range</param>
+        /// <param name="to">To date for the search range</param>
+        /// <param name="parameters">Optional query parameters</param>
+        /// <returns>The collection objec containing the found messages</returns>
+        public DynamicJsonObject SearchMessages(string streamName, DateTime from, DateTime to, Dictionary<string,object> parameters)
+        {
+            SearchParameters searchParameters = new SearchParameters(parameters);
+            return this.SearchMessages(streamName, from, to, searchParameters);
+        }
+
+        /// <summary>
+        /// Searches for Messages
+        /// </summary>
+        /// <param name="streamName">The name of the stream in which to search messages</param>
+        /// <param name="from">From date for the search range</param>
+        /// <param name="to">To date for the search range</param>
+        /// <param name="parameters">Optional query parameters</param>
+        /// <returns>The collection objec containing the found messages</returns>
+        public DynamicJsonObject SearchMessages(string streamName, DateTime from, DateTime to, SearchParameters parameters)
+        {
+            return this.SearchMessages(streamName, from, to, parameters, this.TotalAttempts, this.BaseRetryIntervallMilliseconds);
+        }
+
+        /// <summary>
+        /// Searches for Messages
+        /// </summary>
+        /// <param name="streamName">The name of the stream in which to search messages</param>
+        /// <param name="from">From date for the search range</param>
+        /// <param name="to">To date for the search range</param>
+        /// <param name="parameters">Optional query parameters</param>
+        /// <param name="totalAttempts">Total attempts that are made for a request</param>
+        /// <param name="baseWaitingMilliseconds">Default base retry intervall milliseconds in job polling</param>
+        /// <returns>The collection objec containing the found messages</returns>
+        public DynamicJsonObject SearchMessages(string streamName, DateTime from, DateTime to, object parameters,
+            int totalAttempts, int baseRetryIntervallMilliseconds)
+        {
+            Contract.Assert((parameters is SearchParameters) || (parameters is Dictionary<string, object>), "Parameters format is not suported (valid formats are 'SearchParameters' or 'Dictionary<string,object>'");
+
+            DynamicJsonObject result = null;
+            if (parameters is SearchParameters)
+            {
+                result = this.SearchMessages(streamName, from, to, (SearchParameters)parameters, totalAttempts, baseRetryIntervallMilliseconds);
+            }
+            else if (parameters is Dictionary<string, object>)
+            {
+                result = this.SearchMessages(streamName, from, to, (Dictionary<string, object>)parameters, totalAttempts, baseRetryIntervallMilliseconds);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Searches for Messages
+        /// </summary>
+        /// <param name="streamName">The name of the stream in which to search messages</param>
+        /// <param name="from">From date for the search range</param>
+        /// <param name="to">To date for the search range</param>
+        /// <param name="parameters">Optional query parameters</param>
+        /// <param name="totalAttempts">Total attempts that are made for a request</param>
+        /// <param name="baseWaitingMilliseconds">Default base retry intervall milliseconds in job polling</param>
+        /// <returns>The collection objec containing the found messages</returns>
+        public DynamicJsonObject SearchMessages(string streamName, DateTime from, DateTime to, Dictionary<string, object> parameters,
+            int totalAttempts, int baseRetryIntervallMilliseconds)
+        {
+            SearchParameters searchParameters = new SearchParameters(parameters);
+            return this.SearchMessages(streamName, from, to, searchParameters, totalAttempts, baseRetryIntervallMilliseconds);
+        }
+
+        /// <summary>
+        /// Searches for Messages
+        /// </summary>
+        /// <param name="streamName">The name of the stream in which to search messages</param>
+        /// <param name="from">From date for the search range</param>
+        /// <param name="to">To date for the search range</param>
+        /// <param name="parameters">Optional query parameters</param>
+        /// <param name="totalAttempts">Total attempts that are made for a request</param>
+        /// <param name="baseWaitingMilliseconds">Default base retry intervall milliseconds in job polling</param>
+        /// <returns>The collection objec containing the found messages</returns>
+        public DynamicJsonObject SearchMessages(string streamName, DateTime from, DateTime to, SearchParameters parameters, 
+            int totalAttempts, int baseRetryIntervallMilliseconds)
         {
             #region Contract
+            Contract.Requires(this.IsLoggedIn, "Not logged in, call method login first");
             Contract.Requires(!string.IsNullOrEmpty(streamName), "No tenant id defined");
             Contract.Requires(DateTime.MinValue != from, "From date can not be min date");
             Contract.Requires(DateTime.MaxValue != from, "From date can not be max date");
             Contract.Requires(DateTime.MinValue != to, "To date can not be min date");
             Contract.Requires(DateTime.MaxValue != to, "To date can not be max date");
-            //ToDo: Check if IsLoggedIn=true
             #endregion Contract
 
+            //Add Stream filter
             //ToDo: map tenant ID to streamId
-            string streamQuery = string.Format("streams=\"{0}\"", streamName);
-            if (!string.IsNullOrEmpty(query))
+            string streamQuery = string.Format("streams:\"{0}\"", streamName);
+
+            //Add optional query parameters
+            string optionalParameters = "";
+            if (null != parameters)
             {
-                streamQuery = string.Format("({0}) AND ({1})", streamQuery, query);
+                if (!string.IsNullOrEmpty(parameters.Query))
+                {
+                    streamQuery = string.Format("({0}) AND ({1})", streamQuery, HttpUtility.UrlEncode(parameters.Query));
+                }
+                if (parameters.Limit > 0)
+                {
+                    optionalParameters = string.Format("{0}&limit={1}", optionalParameters, parameters.Limit);
+                }
+                if (parameters.Offset > 0)
+                {
+                    optionalParameters = string.Format("{0}&offset={1}", optionalParameters, parameters.Offset);
+                }
+                if (!string.IsNullOrEmpty(parameters.Filter))
+                {
+                    optionalParameters = string.Format("{0}&filter={1}", optionalParameters, HttpUtility.UrlEncode(parameters.Filter));
+                }
+                if ((null != parameters.FieldNames) && (parameters.FieldNames.Count > 0))
+                {
+                    optionalParameters = string.Format("{0}&fields={1}", optionalParameters, HttpUtility.UrlEncode(string.Join(",", parameters.FieldNames)));
+                }
+                if (!string.IsNullOrEmpty(parameters.SortFieldName))
+                {
+                    string sortParameter = null;
+                    switch (parameters.SortOrder)
+                    {
+                        case SortOrder.Ascending:
+                            sortParameter = string.Format("{0}:asc", parameters.SortFieldName);
+                            break;
+                        case SortOrder.Descending:
+                            sortParameter = string.Format("{0}:desc", parameters.SortFieldName);
+                            break;
+                    }
+                    optionalParameters = string.Format("{0}&sort={1}", optionalParameters, HttpUtility.UrlEncode(sortParameter));
+                }
             }
-            string url = string.Format("{0}?query={1}&from={2}&to={3}", GraylogClient.MessagesAbsoluteDateRangeUrl, HttpUtility.UrlEncode(streamQuery), HttpUtility.UrlEncode(from.ToString(GraylogClient.URL_PARAMETER_DATE_FORMAT)), HttpUtility.UrlEncode(to.ToString(GraylogClient.URL_PARAMETER_DATE_FORMAT)));
-            if (limit > 0)
-            {
-                url = string.Format("{0}&limit={1}", url, limit);
-            }
-            if (offset > 0)
-            {
-                url = string.Format("{0}&offset={1}", url, offset);
-            }
+            string url = string.Format("{0}?query={1}&from={2}&to={3}{4}", GraylogClient.MessagesAbsoluteDateRangeUrl,
+                HttpUtility.UrlEncode(streamQuery), HttpUtility.UrlEncode(from.ToString(GraylogClient.URL_PARAMETER_DATE_FORMAT)),
+                HttpUtility.UrlEncode(to.ToString(GraylogClient.URL_PARAMETER_DATE_FORMAT)), optionalParameters);
+
             HttpRequestHandler requestHandler = new HttpRequestHandler(this.GraylogUrl, this.Username, this.Password);
             DynamicJsonObject result = requestHandler.MakeRequest<DynamicJsonObject>(url, HttpMethod.Get, null, totalAttempts, baseRetryIntervallMilliseconds);
 
