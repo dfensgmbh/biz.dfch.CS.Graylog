13760c1d74039aa4b723b7039f267250e9a51fa2
Niklaus Jordi <jordi@afca.ch>
2016-01-11 15:16:02 +0100

    #2 Clean up references in biz.dfch.CS.Graylog.Api
    * removed not needed nuget packages
    
    resolves #2

--- src/biz.dfch.CS.Graylog.Api/App_Start/RouteConfig.cs
+++ src/biz.dfch.CS.Graylog.Api/App_Start/RouteConfig.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace biz.dfch.CS.Graylog.Api
@@ -11,13 +10,13 @@ namespace biz.dfch.CS.Graylog.Api
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            //routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
         }
     }
 }
